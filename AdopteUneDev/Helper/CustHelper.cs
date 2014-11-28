a514e7fd0ec9a80e3769052c2cf7e691b8cde6dd
mike_person <michael.person@cognitic.be>
2014-11-28 12:14:57 +0100

    Ajout de l'HTML Helper BoldTitle

---
+++ AdopteUneDev/Helper/CustHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdopteUneDev.Helper
+{
+    public static class CustHelper
+    {
+        public static MvcHtmlString BoldTitle(this HtmlHelper Origin, string texte, string laclasse)
+        {
+            TagBuilder ta = new TagBuilder("h1");
+            ta.InnerHtml = texte.ToUpper();
+            ta.AddCssClass(laclasse);
+
+            return new MvcHtmlString(ta.ToString());
+        }
+    }
+}
